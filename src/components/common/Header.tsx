f242ff9e75e9f4907381ead1ec62b3a819ec6e22
jiyong_park <jiyong_park@tmax.co.kr>
2021-01-13 19:54:38 +0900

    1차 template

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -20,18 +20,18 @@ function Header() {
                 </button>
                 <nav className='gnb no-drag'>
                     <ul className='gnbList'>
-                        <div className='menuList spotLight'>
-                            {t('hello')}
-                            <div className='spotInner, inner01'>
+                        <ul className='menuList'>
+                            {t('MENU_TITLE_01')}
+                            <div className='spotInner inner01'>
                                 <li>스포트 12 ~ 15W</li>
                                 <li>스포트 21 ~ 25W</li>
                                 <li>스포트 30 ~ 40W</li>
                                 <li>스포트 50W</li>
                             </div>
-                        </div>
-                        <div className='menuList downLight'>
-                            LED 다운라이트
-                            <div className='spotInner, inner02'>
+                        </ul>
+                        <ul className='menuList'>
+                            {t('MENU_TITLE_02')}
+                            <div className='spotInner inner02'>
                                 <li>45타공 이하</li>
                                 <li>55 ~ 65 타공</li>
                                 <li>70 ~ 80 타공</li>
@@ -42,16 +42,16 @@ function Header() {
                                 <li>105 ~ 125 타공</li>
                                 <li>130 타공이상</li>
                             </div>
-                        </div>
-                        <div className='menuList spotLight'>
-                            할로겐 다운라이트
-                        </div>
-                        <div className='menuList spotLight'>
-                            Product
-                        </div>
-                        <div className='menuList spotLight'>
-                            HOT SALE
-                        </div>
+                        </ul>
+                        <ul className='menuList'>
+                            {t('MENU_TITLE_03')}
+                        </ul>
+                        <ul className='menuList'>
+                            {t('MENU_TITLE_04')}
+                        </ul>
+                        <ul className='menuList'>
+                            {t('MENU_TITLE_05')}
+                        </ul>
                     </ul>
                 </nav>
             </div>
